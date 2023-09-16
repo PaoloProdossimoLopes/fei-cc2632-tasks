e1faee99dbe73c32fa3845aeea8d7c4311cfea64
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-09-16 19:47:49 -0300

    feat: create a task model

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Task {
+    int priority;
+    char *description;
+    char *category;
+} Task;
+
+Task *init_task(int priority, char *description, char *category) {
+    Task *task = malloc(sizeof(Task));
+    task->priority = priority;
+    task->description = description;
+    task->category = category;
+    return task;
+}
+
+void print_task(Task task) {
+    printf(":::: TASK ::::\n");
+    printf("Priority: %d\n", task.priority);
+    printf("Description: %s\n", task.description);
+    printf("Category: %s\n", task.category);
+    printf("==============\n");
+}
 
 int main() {
-    printf("Hello, World!\n");
+    Task *task = init_task(0, "any description", "any category");
+    print_task(*task);
     return 0;
 }
