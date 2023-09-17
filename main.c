66694845355c151e0d96d769e636b426d7cc6352
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-09-16 21:31:12 -0300

    feat: receive a task input

--- main.c
+++ main.c
@@ -1,12 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include "models/task/Task.h"
 
-int main() {
-    Task *task = init_task(0, "any description", "any category");
-    if (!task) exit(1);
+int read_int(char *message) {
+    int input;
+    printf("%s", message);
+    int status = scanf("%d", &input);
+    if (status < 0)
+        return status;
+    return input;
+}
 
-    print_task(*task);
+void read_str(char *message, char *str) {
+    fflush(stdin);
+    printf("%s", message);
+    fgets(str, sizeof(str), stdin);
+}
+
+void generate_menu() {
+    printf("::::::::: MENU :::::::::\n");
+    printf(":: 1 - Create task\n");
+    printf("::::::::::::::::::::::::\n");
+}
+
+int main() {
+    while (true) {
+        generate_menu();
+
+        const int input = read_int("\nWhat you want to do? ");
+
+        const int is_create_menu_option = input == 1;
+        if (is_create_menu_option) {
+            const int task_priority = read_int("Choose the task priority: ");
+
+            char task_description[100];
+            read_str("Choose the task description: ", task_description);
+
+            char task_category[100];
+            read_str("Choose the task category: ", task_category);
+
+            Task *task = init_task(task_priority, task_description, task_category);
+            if (!task) exit(-1);
+
+            print_task(*task);
+        } else {
+            printf(":: ERROR :: Choose a valid option, try again\n");
+            continue;
+        }
+    }
     return 0;
 }
